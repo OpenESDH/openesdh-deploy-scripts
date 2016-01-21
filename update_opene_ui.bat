4cb3efa183568042598962ef2d2a78302e6649d0
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-01-21 09:58:16 +0200

    Included update of the tenants' UI.

--- update_opene_ui.bat
+++ update_opene_ui.bat
@@ -4,6 +4,19 @@ SET UPDATE_MARKER_FILE=C:\opene_updates\opene_ui\update.txt
 
 IF NOT EXIST %UPDATE_MARKER_FILE% EXIT 1
 
+rem Updating tenants' UI
+cd c:\inetpub\visma_dk
+git pull
+call npm install
+call gulp build
+
+cd c:\inetpub\magenta_dk
+git pull
+call npm install
+call gulp staff-install
+call gulp staff build
+
+
 cd c:\OpeneUI
 
 git pull
@@ -13,7 +26,11 @@ call bower update
 
 del %UPDATE_MARKER_FILE%
 call gulp all-modules-install
-gulp all-modules build
+
+gulp --title="Visma OpenE" --logo "./app/assets/images/logo-opene.svg" all-modules build
+
+rem copy additional files
+copy C:\opene_update_scripts\custom\logo-opene.svg C:\OpeneUI\app\assets\images\logo-opene.svg
 
 :ex
-EXIT 0
+rem EXIT 0
