3fd44e22f7a20743b0ceaf57b7895068a190fcd5
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-02-29 09:29:34 +0200

    Updated to include tenant modules.

--- update_opene_ui.bat
+++ update_opene_ui.bat
@@ -4,22 +4,9 @@ SET UPDATE_MARKER_FILE=C:\opene_updates\opene_ui\update.txt
 
 IF NOT EXIST %UPDATE_MARKER_FILE% EXIT 1
 
-rem Updating tenants' UI
-cd c:\inetpub\visma_dk
-git pull
-call npm install
-call gulp build
-
-cd c:\inetpub\magenta_dk
-git pull
-call npm install
-call gulp staff-install
-call gulp staff build
-
-
 cd c:\OpeneUI
 
-git pull
+call git pull
 
 call npm install
 call bower update
@@ -27,10 +14,25 @@ call bower update
 del %UPDATE_MARKER_FILE%
 call gulp all-modules-install
 
-gulp --title="Visma OpenE" --logo "./app/assets/images/logo-opene.svg" all-modules build
+call gulp --title="Visma case" --logo "./app/assets/images/VismaCase.svg" all-modules build
 
 rem copy additional files
-copy C:\opene_update_scripts\custom\logo-opene.svg C:\OpeneUI\app\assets\images\logo-opene.svg
+copy C:\opene_update_scripts\custom\VismaCase.svg C:\OpeneUI\app\assets\images\VismaCase.svg
+
+
+rem Updating tenants' UI
+cd c:\inetpub\visma_dk
+call git pull
+call npm install
+call gulp projectRooms-install
+call gulp projectRooms build
+
+cd c:\inetpub\magenta_dk
+call git pull
+call npm install
+call gulp staff-install
+call gulp staff build
+
 
 :ex
 rem EXIT 0
